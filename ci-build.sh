cb77bb49ce8756bd95f456708233ba045c772968
Aashish-cyber <ranaaashish000@gmail.com>
2026-08-16 07:11:46 +0000

    Add CI Build Script

---
+++ ci-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "==================="
+echo "Building CI Build"
+echo "=================="
+
+echo "Building Java Application with Maven"
+mvn clean package
+
+echo "checking war file"
+ls -lh /target/*.war
+
+echo "Buiilding Docker Image"
+docker build -t devops-java-app:ci .
+
+echo "====================================="
+echo "CI BUILD SUCCESSFUL"
+echo "====================================="
